e57219223a9da80e7e239a2109e06983ee8ef6ba
aidxelsystems <aidxelsystems@gmail.com>
2026-01-05 21:26:58 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * Config for ConfirmForm
+ */
+const CONFIG = {
+    // Replace this URL with your deployed Google Apps Script Web App URL
+    // GASのデプロイ後に発行されるURLをここに設定してください
+    GAS_API_URL: 'https://script.google.com/macros/s/AKfycbwruZ-Khc-a7ggJ9efAXC77A3Nb0uFMDTUONt6oiFVq_tPEJmTP4BHDfSinLk5aqh7Yrg/exec'
+};

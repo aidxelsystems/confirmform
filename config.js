68d02d0f99ee3c8a35681b4dd12a0fa461806f89
aidxelsystems <aidxelsystems@gmail.com>
2026-01-05 21:50:28 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 const CONFIG = {
     // Replace this URL with your deployed Google Apps Script Web App URL
     // GASのデプロイ後に発行されるURLをここに設定してください
-    GAS_API_URL: 'https://script.google.com/macros/s/AKfycby_heafxEtp0SkC5y2HaCSqSZtQq8ArekACmS1cUbaUC832IwwwDTGeoxGQsFmm5UfXqw/exec'
+    GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzrZNH6951PpdGYRffELSXhtbifBKOimrEIXlvH-Sm64i_Mn6Bn11HLhJRI9Gvt2RhPcw/exec'
 };
